adddfcd3cb994f0f2cbb771b98c6c66e6692bf2d
Enock Tangus <tanguskipngeno@gmail.com>
2025-04-12 08:45:28 +0300

    fix onSort event

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -6,11 +6,11 @@ export default function ExpenseTable({ expenses, onDelete, onSort }) {
         <table className="table table-hover">
           <thead className="table-dark">
             <tr>
-              <th onClick={() => onSort('name')} style={{cursor: 'pointer'}}>Expense</th>
-              <th onClick={() => onSort('description')} style={{cursor: 'pointer'}}>Description</th>
-              <th onClick={() => onSort('category')} style={{cursor: 'pointer'}}>Category</th>
-              <th onClick={() => onSort('amount')} style={{cursor: 'pointer'}}>Amount (Ksh)</th>
-              <th onClick={() => onSort('date')} style={{cursor: 'pointer'}}>Date</th>
+              <th style={{cursor: 'pointer'}} onClick={() => onSort('name')}>Expense</th>
+              <th style={{cursor: 'pointer'}} onClick={() => onSort('description')}>Description</th>
+              <th style={{cursor: 'pointer'}} onClick={() => onSort('category')}>Category</th>
+              <th style={{cursor: 'pointer'}} onClick={() => onSort('amount')}>Amount (Ksh)</th>
+              <th style={{cursor: 'pointer'}} onClick={() => onSort('date')}>Date</th>
               <th>Action</th>
             </tr>
           </thead>
