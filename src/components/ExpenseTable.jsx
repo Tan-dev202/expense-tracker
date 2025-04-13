b3e60fabe8962d466d1e438df7ed7a36ecfe8660
Enock Tangus <tanguskipngeno@gmail.com>
2025-04-13 12:52:20 +0300

    add search tool from App.js & modify sort feature to use select option

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,30 +1,62 @@
 import ExpenseRow from "./ExpenseRow";
 import ExpenseTotal from "./ExpenseTotal";
-export default function ExpenseTable({ expenses, onDelete, onSort }) {
-    return (
+export default function ExpenseTable({ expenses, onDelete, onSort, searchPhrase, onSearch }) {
+  return (
+    <>
+      <div className="mb-3 row">
+        <div className="d-flex align-items-between justify-content-between gap-2">
+          <div className="d-flex mb-0">
+            <input
+              id="expense-search"
+              type="text"
+              className="form-control"
+              style={{ width: "300px" }}
+              placeholder="Search expenses..."
+              value={searchPhrase}
+              onChange={onSearch}
+            />
+          </div>
+          <div className="d-flex justify-content-end gap-2">
+          <label htmlFor="sortSelect" className="mb-0">
+            Sort by:
+          </label>
+          <select
+            id="sortSelect"
+            className="form-select"
+            style={{ width: "200px" }}
+            onChange={(event) => onSort(event.target.value)}
+          >
+            <option value="">-- Choose field --</option>
+            <option value="description">Description</option>
+            <option value="category">Category</option>
+          </select>
+          </div>
+        </div>
+      </div>
       <div className="table-responsive">
         <table className="table table-hover">
           <thead className="table-dark">
             <tr>
-              <th style={{cursor: 'pointer'}} onClick={() => onSort('name')}>Expense</th>
-              <th style={{cursor: 'pointer'}} onClick={() => onSort('description')}>Description</th>
-              <th style={{cursor: 'pointer'}} onClick={() => onSort('category')}>Category</th>
-              <th style={{cursor: 'pointer'}} onClick={() => onSort('amount')}>Amount (Ksh)</th>
-              <th style={{cursor: 'pointer'}} onClick={() => onSort('date')}>Date</th>
+              <th>Expense</th>
+              <th>Description</th>
+              <th>Category</th>
+              <th>Amount (Ksh)</th>
+              <th>Date</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {expenses.map(expense => (
-              <ExpenseRow 
-                key={expense.id} 
-                expense={expense} 
-                onDelete={onDelete} 
+            {expenses.map((expense) => (
+              <ExpenseRow
+                key={expense.id}
+                expense={expense}
+                onDelete={onDelete}
               />
             ))}
           </tbody>
           <ExpenseTotal expenses={expenses} />
         </table>
       </div>
-    );
-  }  
+    </>
+  );
+}
